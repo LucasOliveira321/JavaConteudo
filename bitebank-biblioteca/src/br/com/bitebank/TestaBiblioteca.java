857b8ab55323cc5bce228a0a92414714c62c9341
Lucas Oliveira <107444159+LucasOliveira321@users.noreply.github.com>
2023-03-20 20:04:16 -0300

    Aula Java Criando uma Bibliotec

---
+++ bitebank-biblioteca/src/br/com/bitebank/TestaBiblioteca.java
@@ -0,0 +1,13 @@
+package br.com.bitebank;
+
+import br.com.bytebank.modeloTest.*;
+
+public class TestaBiblioteca {
+
+	public static void main(String[] args) {
+		
+		Conta cc = new ContaCorrente(111,12345);
+
+	}
+
+}
